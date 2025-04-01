7abdb7066fcfc001f71558da83e300f2018078ea
Moritz Schalle <schallemoritz188@gmail.com>
2025-04-01 13:26:38 +0200

    add stats.py add count_words_in_string

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words_in_string
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
@@ -5,7 +7,7 @@ def get_book_text(file_path):
     
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(count_words_in_string(get_book_text("books/frankenstein.txt")), "words found in the document")
 
 if __name__ == "__main__":
     main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words_in_string(string):
+    word_list = string.split()
+    return len(word_list)
